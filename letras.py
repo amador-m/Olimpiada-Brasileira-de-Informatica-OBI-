07c94e558035430252ace73e7b9cabf71bf86ab8
GGSAZAO <gustavo.oliveirasa@outlook.com>
2026-01-28 16:57:55 -0300

    Delete letras.py

--- letras.py
+++
@@ -1,7 +0,0 @@
-# https://olimpiada.ic.unicamp.br/pratique/p1/2014/f1/letra/
-
-letra = input()
-palavras = input().split()
-porcentagem = sum(
-    [1 for palavra in palavras if letra in palavra]) / len(palavras) * 100
-print(f'{porcentagem:.1f}')
